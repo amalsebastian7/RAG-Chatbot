dd538e647fd501b4dca767b5b3ccbc025a6efcb2
Amal <amalpsebastian98@gmail.com>
2026-08-10 14:40:55 +0100

    java-orchastrator setup complete

---
+++ java-orchestrator/src/main/java/com/uoa/javaorchestrator/JavaOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.uoa.javaorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOrchestratorApplication.class, args);
+    }
+
+}
---
+++ java-orchestrator/src/test/java/com/uoa/javaorchestrator/JavaOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoa.javaorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
